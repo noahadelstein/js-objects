565accb3ddd2b858f71ce4e8ced3e4c9f382cbad
noahadelstein <noadelstein@gmail.com>
2016-04-27 18:21:18 -0500

    g

---
+++ js/studentreport.js
@@ -0,0 +1,11 @@
+var message = ' ';
+var student;
+function print(message) {
+  document.write("<section>" + message + "</section>");
+}
+for (var i =0; i<students.length; i++) {
+  student = students[i];
+  message+='<h1>' + student.name + '</h1>';
+  //message += '<p>' + student.track + '</p>';
+}
+print(message);
